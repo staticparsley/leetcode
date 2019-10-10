5ddae488bd32950508fb4580302004a75d02f5f6
Ruben Marin <staticparsley92@gmail.com>
2019-10-10 01:13:47 -0400

    finished twoSum

--- twoSum.js
+++ twoSum.js
@@ -1,8 +1,11 @@
-const twoSum = (arr) => {
+const twoSum = (nums, target) => {
+  
+  let existObj = {};
+  for ( let i = 0; i < nums.length; i++ ) {
+    if ( existObj[target-nums[i]] > -1) return [existObj[target-nums[i]], i];
+      existObj[nums[i]] = i;
+    }
+  }
 
-  let ans 
-  return ans
-}
 
-
-
+console.log(twoSum([2,7,11,15],9);
