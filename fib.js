fc6f7cfdf9919be3067f2199ad3b6639f43974e4
Ruben Marin <staticparsley92@gmail.com>
2019-10-13 17:38:07 -0400

    added some recursion solutions

---
+++ fib.js
@@ -0,0 +1,6 @@
+const fib = (i) => {
+    if(i < 2) return i;
+    return fib(i-1) + fib(i-2);
+}
+
+console.log(fib(7));
---
+++ recursiveRange.js
@@ -0,0 +1,6 @@
+const recursiveRange = (n) => {
+    if(n === 0) return 0;
+    return n + recursiveRange(n-1);
+}
+
+console.log(recursiveRange(5));
